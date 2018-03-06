f2ea2aae9c01543924662e9977cca8706536e4e1
rachelefine <rachelefine@gmail.com>
2018-03-06 16:22:06 -0500

    solution through Add a Single-User Route & View

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const tweetBank = require('../tweetBank');
+var path = require('path');
 
 router.use(express.static('public'))
 
@@ -13,10 +14,13 @@ router.get('/', (req, res) => {
     let tweets = tweetBank.list();
     res.render('index', {tweets: tweets});
     res.sendFile(path.join(__dirname + 'public/stylesheets/style.css'));
+    let names = tweetBank.list();
+})
 
-}),
-
-
-
-
+router.get('/users/:name', function(req, res, next) {
+    var userTweets = tweetBank.find( {name: req.params.name} );
+    res.render( 'index', { tweets: userTweets } );
+    console.log(tweets);
+  });
+  
 module.exports = router;
